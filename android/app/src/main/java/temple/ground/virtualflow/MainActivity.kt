caaf221db2378a93662b15ea41f6fa40e407aa2f
Dmitriy Severov <Dmitriy.Severov@bia-tech.ru>
2022-12-28 18:04:08 +0500

    Add Two Grains

--- android/app/src/main/java/temple/ground/virtualflow/MainActivity.kt
+++ android/app/src/main/java/temple/ground/virtualflow/MainActivity.kt
@@ -4,6 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    /*  Two Grains
+        1.  xml  Create textview, textAlign to center, 80 sp, bold.
+        2.  xml  Create below it nested frame layout.
+        3.  xml  Create two buttons with '<' and '>', 150 dp width.
+        4.  xml  Extract button labels to string resources.
+        5.  xml  Add id's to view elements.
+        6.  kt   Create class fields for view elements.
+        7.  kt   Bind all views to class fields with method findViews().
+        8.  kt   Create static constant for starter number.
+        9.  kt   Setup listeners for buttons and starter text in setupViews().
+        10. kt   Create extension function for TextView to increment/decrement.
+        11. kt   Buttons should increment / decrement number in textview.
+        12. AS   Choose proper build variant, run build and launch in emulator.
+     */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
